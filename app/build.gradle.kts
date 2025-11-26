00ee1de1299aacba46ecd642c187af4264f07506
RubeRoid <www.pascha.ru542@gmail.com>
2025-11-26 21:06:06 +0300

    Update app version to 1.1.2: remove shift button, make shift card clickable, change status text

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.bestapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
